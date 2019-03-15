3a52a7350961250cfb933404c5284f5393c33a6a
intellif <intellif@intellifdeiMac.local>
2019-03-15 17:30:43 +0800

    finish

---
+++ mysite_login/entryenv.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+source /Volumes/win/git/venv/bin/activate
---
+++ mysite_login/mysite_login/urls.py
@@ -0,0 +1,14 @@
+# mysite_login/urls.py
+
+from django.conf.urls import url, include
+from django.contrib import admin
+from login import views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^index/', views.index),
+    url(r'^login/', views.login),
+    url(r'^register/', views.register),
+    url(r'^logout/', views.logout),
+    url(r'^captcha', include('captcha.urls')),
+]
